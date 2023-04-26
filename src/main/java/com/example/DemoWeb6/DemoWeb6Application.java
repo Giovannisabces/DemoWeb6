c8350f9493d90b739d9efa23d392551e47ac6215
Giovanni Sabogal Cespedes <giovannisc22@gmail.com>
2023-04-26 13:48:28 +0200

    Demo Web

---
+++ src/main/java/com/example/DemoWeb6/DemoWeb6Application.java
@@ -0,0 +1,13 @@
+package com.example.DemoWeb6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeb6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeb6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoWeb6/DemoWeb6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoWeb6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeb6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
